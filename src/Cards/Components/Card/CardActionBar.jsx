2cac6892491a8711fa23ec80b79f8f262c4d4aab
Praynat <80518903+Praynat@users.noreply.github.com>
2024-03-17 18:00:31 +0200

    businees card app basis

---
+++ src/Cards/Components/Card/CardActionBar.jsx
@@ -0,0 +1,39 @@
+import { Box, CardActions, IconButton } from '@mui/material';
+import DeleteIcon from '@mui/icons-material/Delete';
+import ModeEditIcon from '@mui/icons-material/ModeEdit';
+import CallIcon from '@mui/icons-material/Call';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import React from 'react'
+
+export default function CardActionbar({handleCardDelete,handleCardLike,cardId}) {
+  const handleCardEdit = (id)=>{
+    console.log("navigate to edit page for the card"+ id);
+  }
+  return (
+    <>
+    <CardActions sx={{ paddingTop: 0, justifyContent: "space-between" }}>
+      <Box>
+        <IconButton onClick={()=>{
+          handleCardDelete(cardId)
+        }}>
+          <DeleteIcon />
+        </IconButton>
+        <IconButton onClick={()=> handleCardEdit(cardId)}>
+          <ModeEditIcon />
+        </IconButton>
+      </Box>
+      <Box>
+        <IconButton>
+          <CallIcon />
+        </IconButton>
+        <IconButton >
+          <FavoriteIcon onClick={()=>{
+          handleCardLike(cardId)
+        }}/>
+        </IconButton>
+      </Box>
+    </CardActions>
+
+    </>
+  )
+}
