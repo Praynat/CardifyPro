73a721caeabae5c3789bdc75fc4700bc5ceb23a5
Praynat <80518903+Praynat@users.noreply.github.com>
2024-05-21 00:44:16 +0300

    searchpage update

--- src/layout/header/topNavBar/right-navigation/Search/SearchPage.jsx
+++ src/layout/header/topNavBar/right-navigation/Search/SearchPage.jsx
@@ -1,37 +1,49 @@
-import CardsFeedback from '../../../../../Cards/Components/CardsFeedback';
+import React, { useEffect, useMemo } from 'react'
+import CardsFeedback from '../../../../../Cards/Components/CardsFeedback'
 import useCards from '../../../../../Cards/hooks/useCards';
 import { Box } from '@mui/material';
 import { useMySearch } from './Provider/SearchProvider';
+import { useMyUser } from '../../../../../users/providers/UserProvider';
 
 export default function SearchPage() {
-  const { cards, error, isLoading, handleCardDelete, handleCardLike } = useCards();
-  const { selected, searchInput, filteredAllCards, filteredMyCards, filteredFavCards } = useMySearch();
+    const { cards,error,isLoading, getAllCards,handleCardDelete,handleCardLike} = useCards();
+    const { selected, searchInput } = useMySearch();
+    const {user}=useMyUser();
 
-  const filteredCards = () => {
-    if (!searchInput) {
-      return cards;
-    }
-    if (selected === "My Cards") {
-      return filteredMyCards;
-    }
-    if (selected === "Favorites") {
-      return filteredFavCards;
-    }
-    return filteredAllCards;
-  };
+  useEffect(()=>{
+    getAllCards();
+  }, [getAllCards]);
+      const filteredCards = useMemo(() => {
+        if (!searchInput) return cards;
+        if (selected === "My Cards") {
+          return cards.filter((card) =>
+            card.title.toLowerCase().includes(searchInput.toLowerCase())&&
+          card.userId === user._id
+          );
+        }
+        if (selected === "Favorites") {
+          return cards.filter((card) =>
+            card.title.toLowerCase().includes(searchInput.toLowerCase())&&
+          card.likes.includes(user._id)
+          );
+        } 
+        return cards.filter((card) =>
+          card.title.toLowerCase().includes(searchInput.toLowerCase())
+        );
+      }, [cards, selected, searchInput, user._id]);
+    
 
- 
   return (
-    <Box sx={{ width: "100%" }}>
-      <CardsFeedback
-        cards={filteredCards()}
+    <Box sx={{width:"100%"}}>
+        <CardsFeedback 
+        cards={filteredCards}
         isLoading={isLoading}
         error={error}
         handleCardDelete={handleCardDelete}
         handleCardLike={handleCardLike}
         selected={selected}
         searchInput={searchInput}
-      />
+        /> 
     </Box>
-  );
+    )
 }
